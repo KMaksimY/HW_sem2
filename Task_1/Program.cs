6b641a40344d5e463cba344194056dea48dc0895
Maksim Komarov <komarovmu@gmail.com>
2023-04-04 00:48:06 +0100

    Task_1_HW2

---
+++ Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+  Console.WriteLine("Put two numbers");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if (a>b){
+  Console.WriteLine("a more then b");
+}
+else if (a<b)
+{
+  Console.WriteLine("b more then a");
+  }
+else
+{
+  Console.WriteLine("a and b the same");
+}
+}  
+}
+
