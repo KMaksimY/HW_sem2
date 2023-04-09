4f3ffe9fc9b6ddfce6106b0583c4eaac97e67ebd
Maksim <komarovmu@gmail.com>
2023-04-09 23:53:35 +0100

    add Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Put one number");
+
+float a = Convert.ToInt32(Console.ReadLine ());
+
+if (a%2==0){
+
+Console.WriteLine("четное");
+}
+
+else {
+Console.WriteLine("не четное");
+
+}
+    }
+}
+
+
+
+
